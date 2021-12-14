752004fae591e4b211e5a87a757ae06844f9f2d2
Felipe Zintzun <felipemendoza@felipes-mbp.lan>
2021-12-13 21:28:51 -0600

    added search card

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,22 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert ("Please enter a GitHub username")
+  }
+  console.log(event);
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -6,9 +25,19 @@ var getUserRepos = function(user) {
     fetch(apiUrl).then(function(response) {
       console.log(response);
       response.json().then(function(data) {
-        console.log(data);
+        displayRepos(data, user);
       });
     });
   };
+
+  var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+  };
   
-  getUserRepos("lernantino");
+  // getUserRepos("lernantino");
+  userFormEl.addEventListener("submit", formSubmitHandler);
